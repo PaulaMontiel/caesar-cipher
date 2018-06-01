15e623a2d603bd62b9e4c854300a8aa23dd4e17f
Paula Montiel <p_dany_mr@hotmail.com>
2018-06-01 14:11:35 -0400

    tratando de que sea el ultimo cambio >.<

--- cipher.js
+++ cipher.js
@@ -1,23 +1,29 @@
-window.cipher = {
+cipher = {
   encode: () => {
-   function caesar(str, offset) {
-        const output = " ";
-        offset = parseInt(offset);
-        for (let i=0; i<str.length;i++){
-        output += String.fromCharCode(str.charCodeAt(i)- 65 + offset )% 26 + 65;
-        }
-        return output;    
+    str = document.getElementById("input1").value.toUpperCase(); 
+    cifrado1 = document.getElementById("cifrado1");
+    offset = document.getElementById("espacio1").value;
+    var output="";
+    clave = parseInt(offset);
+    for (var i=0; i<str.length;i++){
+		output += String.fromCharCode(str.charCodeAt(i)+clave);
     }
+    cifrado1.value = output;
+    return output;  
   },
   decode: () => {
-    function caesar(str, offset) {
-      const output = " ";
-      offset = parseInt(offset);
-      for (let i=0; i<str.length;i--){
-      output -= String.fromCharCode(str.charCodeAt(i)- 65 - offset )% 26 + 65 ;
-      }
-      return output;    
-  }
+    str = document.getElementById("input2").value.toUpperCase(); 
+    cifrado2 = document.getElementById("cifrado2");
+    offset = document.getElementById("espacio2").value;
+    offset = offset * -1; 
+
+    var output="";
+    clave = parseInt(offset);
+    for (var i=0; i<str.length;i++){
+		output += String.fromCharCode(str.charCodeAt(i)+clave);
+    }
+	  // All done!
+    cifrado2.value = output;
+    return output;
   }
 }
-caesar();
