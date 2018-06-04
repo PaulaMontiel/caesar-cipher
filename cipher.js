0a52242714047983fefc23df8dc6033893fc37b6
Paula Montiel <p_dany_mr@hotmail.com>
2018-06-04 15:16:14 -0400

    Initial src subtree commit

--- cipher.js
+++ cipher.js
@@ -2,14 +2,19 @@ cipher = {
   encode: () => {
     str = document.getElementById("input1").value.toUpperCase(); 
     cifrado1 = document.getElementById("cifrado1");
-    offset = document.getElementById("espacio1").value;
-    var output="";
-    clave = parseInt(offset);
-    for (var i=0; i<str.length;i++){
-		output += String.fromCharCode(str.charCodeAt(i)+clave);
-    }
-    cifrado1.value = output;
-    return output;  
+    offset = parseInt(document.getElementById("espacio1").value);
+    var salida = "";
+      var oldASCII; //donde se guarda el codigo ascii de una letra
+      var newASCII;//codigo ascii resultante luego de sumarle shiftedpositions
+      //por cada letra de la entrada
+      for(var c = 0; c < str.length; c++){
+        oldASCII = str[c].charCodeAt();//obtenemos su codigo
+        newASCII = oldASCII + offset;//desplazamos de lugar la letra al sumarle shiftedpositions
+        salida = salida.concat(String.fromCharCode(newASCII));//convertimos el nuevo codigo a string y concatenamos
+      }
+      cifrado1.value = salida;
+      return salida;
+
   },
   decode: () => {
     str = document.getElementById("input2").value.toUpperCase(); 
