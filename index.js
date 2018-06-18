a2c7e423fea9e564faea01bd4b0b898f39d350f0
Paula Montiel <p_dany_mr@hotmail.com>
2018-06-18 01:00:13 -0400

    reestructurando trabajo

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-/* Acá va tu código */
+document.getElementById("encode").addEventListener("click", () => {
+    const output = document.getElementById('str').value;
+    const offset = document.getElementById('offset').value;
+    document.getElementById('cifrado').innerHTML = cipher.encode(output, offset);
+});
+document.getElementById("decode").addEventListener("click", () => {
+    const output2 = document.getElementById('str2').value;
+    const offset2 = document.getElementById('offset2').value;
+    document.getElementById('descifrado').innerHTML = cipher.decode(output2, offset2);
+});
